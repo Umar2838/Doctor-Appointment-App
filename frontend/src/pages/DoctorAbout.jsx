b460a860290cb7c961ea070bdd6c521feaea0bef
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2024-01-20 01:16:02 +0500

    another cahnges

---
+++ frontend/src/pages/DoctorAbout.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { formatDate } from "../utils/formatDate"
+const DoctorAbout = () => {
+    return (
+        <div>
+
+            <div>
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold lex items-center gap-2' >About of
+                    <span className='text-irisBlue font-bold text-[24px] leading-9' >Munibur Rehman</span>
+                </h3>
+                <p className="text__para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae explicabo quaerat soluta eveniet quis architecto corporis debitis obcaecati, blanditiis accusamus fuga voluptas, itaque perferendis rem?</p>
+            </div>
+            <div className='mt-12' >
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold lex items-center gap-2' >Education</h3>
+                <ul className='pt-4 md:p-5' >
+                    <li className='flex flex-col sm:flex-row sm:justify-between sm:item-end md:gap-5 mb-[30px] ' >
+                        <div>
+                            <span className='text-irisBlueColor text-[15px] leading-6 font-semibold' >{formatDate("06-21-2008")} - {formatDate("06-21-20012")}</span>
+                            <p className='text-[16px] leading-6 font-medium text-textColor' >PHD in Surgeon</p>
+                        </div>
+                        <p className='text-[14px] leading-6 font-medium text-textColor' >New Apoloio Hospital, New York</p>
+
+                    </li>
+                    <li className='flex flex-col sm:flex-row sm:justify-between sm:item-end md:gap-5 mb-[30px] ' >
+                        <div>
+                            <span className='text-irisBlueColor text-[15px] leading-6 font-semibold' >{formatDate("12-04-2018")} - {formatDate("06-21-2022")}</span>
+                            <p className='text-[16px] leading-6 font-medium text-textColor' >PHD in Surgeon</p>
+                        </div>
+                        <p className='text-[14px] leading-6 font-medium text-textColor' >New Apoloio Hospital, New York</p>
+
+                    </li>
+                </ul>
+            </div>
+
+            <div className='mt-12' >
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold lex items-center gap-2' >Ecperience</h3>
+
+                <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5' >
+                    <li className='p-4 rounded bg-[#fff9ea]' >
+                        <span className='text-yelloColor text-[15px] leading-6 font-semibold' >{formatDate("06-21-2008")} - {formatDate("06-21-20012")}</span>
+                        <p className='text-[16px] leading-6 font-medium text-textColor' >Sr. Surgeon</p>
+
+                        <p className='text-[16px] leading-6 font-medium text-textColor' >New Apoloio Hospital, New York</p>
+
+
+                    </li>
+
+                    <li className='p-4 rounded bg-[#fff9ea]' >
+                        <span className='text-yelloColor text-[15px] leading-6 font-semibold' >{formatDate("06-21-2008")} - {formatDate("06-21-20012")}</span>
+                        <p className='text-[16px] leading-6 font-medium text-textColor' >Sr. Surgeon</p>
+
+                        <p className='text-[16px] leading-6 font-medium text-textColor' >New Apoloio Hospital, New York</p>
+
+
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default DoctorAbout
--- frontend/src/pages/DoctorsDetail.jsx
+++ frontend/src/pages/DoctorsDetail.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import doctorImg from "../assets/images/doctor-img02.png"
+import starIcon from "../assets/images/Star.png"
+import DoctorAbout from './DoctorAbout'
+import Feedback from './Feedback'
 
 const DoctorsDetail = () => {
+
+  const [tab, setTab] = useState("about")
+
   return (
-    <div>
-      doctordetails
-    </div>
+    <section>
+      <div className='mzx-w-[1170px] px-5 mx-auto' >
+        <div className='grid md:grid-col-3 gap-[50px]' >
+          <div className='md:col-span-3' >
+            <div className='flex item-center gap-5' >
+              <figure className="max-w-[200px] max-h-[200px]" >
+                <img src={doctorImg} className='w-full' />
+              </figure>
+              <div>
+                <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded ' >Surgeon</span>
+                <h3 className='text-headingColor text-[22px] leading-9 mt-3 font-bold'  >Muhib Rehman</h3>
+                <div className='flex items-center gap-[6px]' >
+                  <span className='flex items-centre gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor' >
+                    <img src={starIcon} />4.8
+                  </span>
+                  <span className='text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] text-textColor' >(272)</span>
+                </div>
+                <p className='text__para text-[14px] md:text-[15px] leading-5 lg:max-w-[390px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas nam accusamus ipsum, nemo nostrum!</p>
+              </div>
+
+            </div>
+
+            <div className='mt-[50px] border-solid border-[#0066ff34]' >
+              <button onClick={() => setTab('about')} className={` ${tab == "about" && 'border-b border-solid border-primaryColor'} py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold `} >About</button>
+              <button onClick={() => setTab('feedback')} className={` ${tab == "feedback" && 'border-b border-solid border-primaryColor'}  py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold `} >Feedback</button>
+
+            </div>
+
+            <div className='mt-[50px]' >
+              {
+tab=="about" && <DoctorAbout/>
+              }
+              {
+tab=="feedback" && <Feedback/>
+              }
+            </div>
+
+          </div>
+
+          <div></div>
+
+
+        </div>
+      </div>
+    </section>
   )
 }
 
