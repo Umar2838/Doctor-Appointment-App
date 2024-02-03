40f5185f4eea0418292ccedaaa9f8759d7350939
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2024-02-04 02:34:32 +0500

    working on backend

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,9 +1,9 @@
 import express from "express"
 import { updateUser,deleteUser,getAllUser,getSingleUser } from "../Controllers/userControllers.js";
-
+import { authenticate , restrict } from "../auth/verifyToken.js";
 const router = express.Router()
 
-router.get("/:id",getSingleUser)
+router.get("/:id",authenticate, restrict(["admin"]),getSingleUser)
 router.get("/",getAllUser)
 router.put("/:id",updateUser)
 router.delete("/:id",deleteUser)
